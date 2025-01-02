2590819f2a228d08d2a0dac31864c7f52a6a07ad
Evgeniy Pelikh <eugene.p9g@gmail.com>
2025-01-02 12:47:15 +0100

    add spacing, change return of schedule_parser method

--- parsing/main.py
+++ parsing/main.py
@@ -6,19 +6,17 @@ DOMAIN = 'http://www.dgma.donetsk.ua'
 URL_CALL_SCHEDULE = 'http://www.dgma.donetsk.ua/13-09-22-rozklad-dzvinkiv.html'
 
 
-def schedule_parser(url):
-    request_schedule = requests.get(url)
+def schedule_parser():
+    request_schedule = requests.get(URL_CALL_SCHEDULE)
     soup = bs(request_schedule.text, 'html.parser')
 
-    # Text
     call_schedule_divs = soup.find_all('div', class_='item-page')
     call_schedule_p = call_schedule_divs[0].find_all('p')
 
-    # Image
     call_schedule_a = call_schedule_divs[0].find_all('a')
     call_schedule_image = call_schedule_a[0].find('img')['src']
 
-    return [p.text for p in call_schedule_p] + ["\n" + (f'{DOMAIN}{call_schedule_image}')]
+    text = [p.text for p in call_schedule_p]
+    image_url = f"{DOMAIN}{call_schedule_image}"
 
-
-print("\n".join(schedule_parser(URL_CALL_SCHEDULE)))
+    return text, image_url, URL_CALL_SCHEDULE
