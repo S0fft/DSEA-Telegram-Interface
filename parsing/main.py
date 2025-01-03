4c659b0f7ed537ada9760a2d1ef07bc937c95a51
Evgeniy Pelikh <eugene.p9g@gmail.com>
2025-01-03 12:48:04 +0100

    create class_schedule_parser method

--- parsing/main.py
+++ parsing/main.py
@@ -4,11 +4,12 @@ from bs4 import BeautifulSoup as bs
 DOMAIN = 'http://www.dgma.donetsk.ua'
 
 URL_CALL_SCHEDULE = 'http://www.dgma.donetsk.ua/13-09-22-rozklad-dzvinkiv.html'
+URL_CLASS_SCHEDULE = 'http://www.dgma.donetsk.ua/rozklad-dlya-dennogo-viddilennya.html'
 
 
 def call_schedule_parser():
-    request_schedule = requests.get(URL_CALL_SCHEDULE)
-    soup = bs(request_schedule.text, 'html.parser')
+    request_call_schedule = requests.get(URL_CALL_SCHEDULE)
+    soup = bs(request_call_schedule.text, 'html.parser')
 
     call_schedule_divs = soup.find_all('div', class_='item-page')
     call_schedule_p = call_schedule_divs[0].find_all('p')
@@ -20,3 +21,34 @@ def call_schedule_parser():
     image_url = f"{DOMAIN}{call_schedule_image}"
 
     return text, image_url, URL_CALL_SCHEDULE
+
+
+def class_schedule_parser():
+    request_schedule = requests.get(URL_CLASS_SCHEDULE)
+    soup = bs(request_schedule.text, 'html.parser')
+
+    schedule_divs = soup.find_all('div', class_='item-page')
+    schedule_h = schedule_divs[0].find('h2')
+
+    schedule_a = schedule_divs[0].find_all('a')
+    schedule_images = []
+
+    for src in schedule_a:
+        image = src.find('img')
+
+        if image and 'src' in image.attrs:
+            schedule_images.append(image['src'])
+
+    return schedule_h.text.strip(), schedule_images, URL_CLASS_SCHEDULE
+
+
+print(class_schedule_parser())
+
+
+# schedule_text, schedule_images = class_schedule_parser()
+
+# print("Title:", schedule_text)
+# print("URL of image:")
+
+# for i, image_url in enumerate(schedule_images, 1):
+#     print(f"{i}. {DOMAIN}{image_url}")
