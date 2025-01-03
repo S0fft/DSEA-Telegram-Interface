d7d6b0c1d583b6a9620deedbf67b7b07871630e0
Evgeniy Pelikh <eugene.p9g@gmail.com>
2025-01-03 12:48:35 +0100

    add beta-method for send_class_schedule

--- bot/main.py
+++ bot/main.py
@@ -2,7 +2,7 @@ import requests
 import telebot
 from decouple import config
 
-from parsing.main import call_schedule_parser
+from parsing.main import call_schedule_parser, class_schedule_parser
 
 TOKEN = config('TOKEN')
 bot = telebot.TeleBot(TOKEN)
@@ -13,14 +13,24 @@ def send_welcome(message):
     bot.reply_to(message, 'Message!')
 
 
-@bot.message_handler(commands=['schedule', 'расписание', 'розклад'])
+@bot.message_handler(commands=['call_schedule'])
 def send_call_schedule(message):
-    schedule_text, image_url, page_url = call_schedule_parser()
+    text, image_url, page_url = call_schedule_parser()
 
-    text = "\n".join(schedule_text) + "\n"*2 + f'Джерело: {page_url}'
+    text = "\n".join(text) + "\n"*2 + f'Джерело: {page_url}'
     response = requests.get(image_url, stream=True)
 
     bot.send_photo(message.chat.id, response.raw, caption=text)
+
+
+# @bot.message_handler(commands=['class_schedule'])
+# def send_class_schedule(message):
+#     text, image_url, page_url = class_schedule_parser()
+
+#     text = "\n".join(text) + "\n"*2 + f'Джерело: {page_url}'
+#     response = requests.get(image_url, stream=True)
+
+#     bot.send_photo(message.chat.id, response.raw, caption=text)
 
 
 bot.polling()
